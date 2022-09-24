988fdebb9a52755dfc1296918429214cabbb17da
iamsarthakm <maheshwarisarthak1998@gmail.com>
2022-09-25 00:18:55 +0530

    Fixed linting errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -13,4 +13,4 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
